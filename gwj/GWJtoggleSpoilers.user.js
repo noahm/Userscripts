035ad8bd996caa464b4a26bb289f9220b67a347c
Noah Manneschmidt <noah@manneschmidt.net>
2016-12-07 00:06:35 -0800

    Add update url, comments, preventDefault

--- gwj/GWJtoggleSpoilers.user.js
+++ gwj/GWJtoggleSpoilers.user.js
@@ -7,18 +7,16 @@
 // @match           https://www.gamerswithjobs.com/node/*
 // @grant           none
 // @run-at          document-end
+// @updateURL       https://github.com/noahm/Userscripts/raw/master/gwj/GWJtoggleSpoilers.user.js
 // ==/UserScript==
 (function() {
 var li;
 var toggled = false;
 
-function toggle() {
+function toggle(e) {
+	e.preventDefault();
 	toggled = !toggled;
-	if (toggled) {
-		li.style.transform = 'rotateY(180deg)';
-	} else {
-		li.style.transform = '';
-	}
+	li.style.transform = toggled ? 'rotateY(180deg)' : '';
 	document.querySelectorAll('.quote.spoiler').forEach(div => {
 		div.classList.toggle('spoiler-shown');
 	});
@@ -26,6 +24,7 @@ function toggle() {
 
 var list = document.querySelector('ul.tabs.primary');
 if (!list) {
+	// Handle complete lack of tabs if user is logged out
 	var tabsContainer = document.querySelector('div.tabs-actions');
 	if (tabsContainer) {
 		var tabs = document.createElement('div');
@@ -36,6 +35,7 @@ if (!list) {
 	}
 }
 if (list) {
+	// insert the new tab
 	li = document.createElement('li');
 	li.innerHTML = '<span><a href="#" title="Toggle All Spoilers">§</a></span>';
 	li.style.display = 'inline-block';
