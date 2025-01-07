759b604e1e4a750cdfd9400a90cce27621996f80
Noah Manneschmidt <noah@manneschmidt.net>
2025-01-07 11:58:13 -0800

    also support autofill when selecting using file inputs

--- ziv/ziv-dragdrop.user.js
+++ ziv/ziv-dragdrop.user.js
@@ -14,7 +14,7 @@
 (async function () {
   "use strict";
   const parser = await import(
-    "https://unpkg.com/simfile-parser@0.8.0/dist/browser/index.js"
+    "https://unpkg.com/simfile-parser@0.8.1/dist/browser/index.js"
   );
   const parseSong = parser.parseSongFolderOrData;
 
@@ -39,31 +39,54 @@
         console.log("no simfile found");
         return;
       }
-      document.querySelector("input[name='simfilename']").value =
-        simfile.title.translitTitleName || simfile.title.titleName;
-      document.querySelector("input[name='simfileartist']").value =
-        simfile.artist;
-      // max min bpm
-      document.querySelector("input[name='minbpm']").value = simfile.minBpm;
-      document.querySelector("input[name='maxbpm']").value = simfile.maxBpm;
-      // charts
-      for (const chart of simfile.availableTypes) {
-        try {
-          const inputName = chart.mode[0] + chart.difficulty.slice(0, 3);
-          document.querySelector(`select[name='${inputName}']`).value =
-            chart.feet;
-        } catch (e) {
-          console.log("error when filling chart ", chart.slug, e);
-        }
-      }
+      autofillSimfile(simfile);
     } catch (e) {
       console.log(e);
     }
-
-    document.querySelector('select[name="mp3quality"]').value =
-      "Perfect (High Quality)";
-    document.querySelector('select[name="backgroundquality"]').value =
-      "Generic";
-    document.querySelector('input[name="autozip"]').checked = true;
   });
+
+  document
+    .querySelector('input[name="DWIfile"]')
+    .addEventListener("change", handleFileInputChange);
+  document
+    .querySelector('input[name="SMfile"]')
+    .addEventListener("change", handleFileInputChange);
+  document
+    .querySelector('input[name="SSCfile"]')
+    .addEventListener("change", handleFileInputChange);
+
+  async function handleFileInputChange(evt) {
+    if (evt.currentTarget.files[0]) {
+      const simfile = await parseSong(evt.currentTarget);
+      if (!simfile) {
+        console.log("no simfile found");
+        return;
+      }
+      autofillSimfile(simfile);
+    }
+  }
 })();
+
+function autofillSimfile(simfile) {
+  document.querySelector("input[name='simfilename']").value =
+    simfile.title.translitTitleName || simfile.title.titleName;
+  document.querySelector("input[name='simfileartist']").value = simfile.artist;
+  // max min bpm
+  document.querySelector("input[name='minbpm']").value = simfile.minBpm;
+  document.querySelector("input[name='maxbpm']").value = simfile.maxBpm;
+  // charts
+  for (const chart of simfile.availableTypes) {
+    try {
+      const inputName = chart.mode[0] + chart.difficulty.slice(0, 3);
+      document.querySelector(`select[name='${inputName}']`).value = chart.feet;
+    } catch (e) {
+      console.log("error when filling chart ", chart.slug, e);
+    }
+  }
+
+  // other fields for QoL
+  document.querySelector('select[name="mp3quality"]').value =
+    "Perfect (High Quality)";
+  document.querySelector('select[name="backgroundquality"]').value = "Generic";
+  document.querySelector('input[name="autozip"]').checked = true;
+}
