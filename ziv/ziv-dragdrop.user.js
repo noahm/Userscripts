34726c04792890bd83e6e68e12d8834e1216f0a4
Noah Manneschmidt <noah@manneschmidt.net>
2025-01-06 19:36:38 -0800

    add ziv drag/drop upload enhancement

---
+++ ziv/ziv-dragdrop.user.js
@@ -0,0 +1,63 @@
+// ==UserScript==
+// @name         Ziv Upload DragDrop
+// @namespace    http://noah.manneschmidt.net/
+// @version      2025-01-07
+// @description  lets you drop a pack directly onto the ziv upload form to autofill some fields
+// @author       Noah Manneschmidt
+// @match        https://zenius-i-vanisher.com/v5.2/uploadsimfile.php?*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=zenius-i-vanisher.com
+// @grant        none
+// @downloadURL  https://github.com/noahm/Userscripts/raw/master/ziv/ziv-dragdrop.user.js
+// @updateURL    https://github.com/noahm/Userscripts/raw/master/ziv/ziv-dragdrop.user.js
+// ==/UserScript==
+
+(async function () {
+  "use strict";
+  const parser = await import(
+    "https://unpkg.com/simfile-parser@0.8.0/dist/browser/index.js"
+  );
+  const parseSong = parser.parseSongFolderOrData;
+
+  document.body.addEventListener("dragover", (evt) => {
+    evt.preventDefault();
+  });
+
+  document.body.addEventListener("drop", async (evt) => {
+    console.log("handle drop");
+    evt.preventDefault();
+    if (!evt.dataTransfer) {
+      return;
+    }
+
+    if (evt.dataTransfer.items.length !== 1) {
+      console.error("too many items dropped");
+      return;
+    }
+    try {
+      const simfile = await parseSong(evt.dataTransfer.items[0]);
+      if (!simfile) {
+        console.log("no simfile found");
+        return;
+      }
+      document.querySelector("input[name='simfilename']").value =
+        simfile.title.translitTitleName || simfile.title.titleName;
+      document.querySelector("input[name='simfileartist']").value =
+        simfile.artist;
+      // max min bpm
+      document.querySelector("input[name='minbpm']").value = simfile.minBpm;
+      document.querySelector("input[name='maxbpm']").value = simfile.maxBpm;
+      // charts
+      for (const chart of simfile.availableTypes) {
+        try {
+          const inputName = chart.mode[0] + chart.difficulty.slice(0, 3);
+          document.querySelector(`select[name='${inputName}']`).value =
+            chart.feet;
+        } catch (e) {
+          console.log("error when filling chart ", chart.slug, e);
+        }
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+})();
